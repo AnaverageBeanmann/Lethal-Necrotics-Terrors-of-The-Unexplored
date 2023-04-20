f427fb2f18f0d579bfaecb02b5f6e0469d47a02f
An average Beanmann <102012974+AnaverageBeanmann@users.noreply.github.com>
2023-04-19 22:29:37 -0400

    Delete lua/entities/npc_vj_totu_weaponized_redead directory

--- lua/entities/npc_vj_totu_weaponized_redead/shared.lua
+++
@@ -1,8 +0,0 @@
-ENT.Base 			= "npc_vj_totu_base_walker" -- List of all base types: https://github.com/DrVrej/VJ-Base/wiki/Base-Types
-ENT.Type 			= "ai"
-ENT.PrintName 		= "Redead"
-ENT.Author 			= "Original NPC by Darkborn, Modified by An average Beanmann"
-ENT.Contact 		= "http://steamcommunity.com/groups/vrejgaming"
-ENT.Purpose 		= "Deimos basic."
-ENT.Instructions 	= "Click on the spawnicon to spawn it."
-ENT.Category		= "Terrors of The Unexplored"
